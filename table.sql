8387618a50f1907e81674470394b031e4e97415f
meowsveta <119702899+meowsveta@users.noreply.github.com>
2023-10-17 12:21:02 +0300

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,265 @@
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 10 м', 10, '2020-02-21');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 100 м', 50, '2010-05-12');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 100 км', 200, '2010-05-15');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 5 км', 9, '2015-04-10');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Международные соревнования по бегу на 1 км', 90, '2018-09-10');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 90 км', 210, '2023-08-22');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 20 км', 5, '2021-07-15');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 50 км', 40, '2017-05-17');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Международные соревнования по бегу на 100 м', 9, '2020-06-04');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 200 м', 15, '2007-06-12');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 400 м', 25, '2019-01-11');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 1 км', 60, '2018-02-24');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 400 м', 15, '2017-03-25');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 10 м', 7, '2021-04-26');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 30 км', 300, '2022-05-24');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 10 км', 100, '2020-06-14');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 800 м', 30, '2023-07-09');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 7 км', 5, '2020-08-08');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 12 км', 12, '2010-09-07');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 17 км', 17, '2011-10-06');
+
+INSERT INTO trainee.t_competition
+(competition_name, world_record, set_date)
+VALUES('Бег на 20 км', 35, '2012-11-05');
+
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Алексей Иванов' , 1 , 1993, 'Russia', 20);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Александр Смирнов' , 2 , 1992, 'Россия', 7);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Андрей Кузнецов' , 3 , 1991, 'Казахстан', 50);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Василий Попов' , 4 , 1990, 'Украина', 15);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Борис Васильев' , 5 , 1989, 'Армения', 30);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Глеб Петров' , 2 , 1987, 'Россия', 50 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Роман Фёдоров' , 3 , 1986, 'Россия', 30);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Матвей Куклачёв' , 4 , 1985, 'Россия', 10 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Михаил Антонов' , 5 , 1983, 'Украина',  6 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Игорь Тарасов' , 1 , 1981, 'Казахстан', 59);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Вадим Титов' , 2 , 1980, 'Беларусь', 182);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Станислав Поляков' , 3 , 1984, 'Беларусь', 200);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Евгений Сорокин' , 4 , 1994, 'Россия', 120 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Алексей Денисов' , 5 , 1993, 'Россия', 20 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Анастасия Фомина' , 1 , 1990, 'Россия', 8 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Дарья Щербакова' , 2 , 1990, 'Россия', 200);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Злата Исаева' , 3 , 1990, 'Россия',  23 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Карина Маслова' , 4 , 2003, 'Россия', 59 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Диана Жукова' , 5 , 2002, 'Россия', 49 );
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Виктория Медведева' , 1 , 2001, 'Россия', 47);
+INSERT INTO trainee.t_sportsman
+(sportsman_name, "rank", year_of_birth, country, personal_record)
+VALUES('Екатерина Белова' , 2 , 2000, 'Россия', 99 );
+
+
+
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(1, 1,  10 , 'Москва', '2022-05-20');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(1, 2, 15 , 'Москва', '2022-05-20');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(1, 3, 20 , 'Москва', '2022-05-20');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(2, 4, 50 , 'Санкт-Петербург', '2020-06-10');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(2, 5, 60 , 'Санкт-Петербург', '2020-06-10');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(2, 6, 45 , 'Санкт-Петербург', '2020-06-10');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(3, 7,  19 , 'Краснодар', '2000-08-25');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(3, 8,  22 , 'Краснодар', '2000-08-25');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(3, 9, 25 , 'Краснодар', '2000-08-25');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(4, 10,  10 , 'Сочи', '2010-07-30');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(4, 11,  11, 'Сочи', '2010-07-30');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(4, 12,12 , 'Сочи', '2010-07-30');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(5, 13,  10 , 'Калининград', '2015-06-12');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(5, 14,  45, 'Калининград', '2015-06-12');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(5, 15, 47 , 'Калининград', '2015-06-12');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(6, 16, 25 , 'Москва', '2015-05-05');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(6, 17,  10 , 'Москва', '2015-05-05');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(7, 18,  99 , 'Нижний Новгород', '1999-10-10');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(7, 19, 80 , 'Нижний Новгород', '1999-10-10');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(8, 20,  100 ,  'Москва', '2000-05-09');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(8, 21,  110, 'Москва', '2000-05-09');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(9, 2,  200 , 'Москва', '2012-09-07');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(9, 1, 10, 'Москва', '2012-09-07');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(10, 5, 17 , 'Москва', '2012-09-07');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(10, 2, 19, 'Москва', '2012-09-07');
+INSERT INTO trainee.t_result
+(competition_id, sportsman_id, "result", city, hold_date)
+VALUES(10, 6, 10, 'Москва', '2012-09-07');
+
+SELECT sportsman_name, rank, year_of_birth, country, personal_record FROM trainee.t_sportsman;
+SELECT competition_name, world_record FROM trainee.t_competition;
+select sportsman_name from trainee.t_sportsman where year_of_birth = '1990';
+select competition_name, world_record from t_competition where set_date = '2010-05-12' OR  set_date =  '2010-05-15';
+select hold_date from t_result where city = 'Москва' and result = 10;
+select sportsman_name from t_sportsman where personal_record != 25;
+select competition_name from t_competition where world_record != 15 and set_date != '2015-12-02';
+select city from t_result where result in (15, 25, 17, 9);
+select sportsman_name from t_sportsman where year_of_birth = 2000 and rank not in (3 , 7 , 9);
+select hold_date from t_result where city like 'М%';
+select competition_name from t_competition where competition_name like '%_еждународные%';
+select distinct  year_of_birth  from t_sportsman;
+select count  (*) from t_result where hold_date = '2014-12-05' ;
+select max(result) from t_result where city ='Москва';
+select min(year_of_birth) from t_sportsman where rank = 1 ;
+select sportsman_name from t_sportsman, t_result tr where personal_record = tr."result" AND tr.hold_date = '2014-12-04';
+select competition_name from t_competition , t_result tr where set_date = '2015-04-20' and tr.city = 'Москва';
+select AVG(personal_record) from t_sportsman;
+select DISTINCT year_of_birth FROM t_sportsman RIGHT JOIN t_result ON t_result.sportsman_id = t_sportsman.sportsman_id WHERE result>(SELECT AVG(result) from t_result) AND city='Москва';
+select sportsman_name from t_sportsman, t_competition tc where year_of_birth > extract (year from tc.set_date)and tc.world_record = 12;
+SELECT 'Спортсмен '  sportsman_name  ' показал результат '  "result"  ' в городе ' || city as sentence FROM t_result r  JOIN sportsman s ON r.sportsman_id = s.sportsman_id;
+select sportsman_name from t_sportsman where  year_of_birth = 2000  and  "rank" > (select AVG("rank") from t_sportsman )
+select * from t_sportsman , t_result tr where personal_record  = tr."result" ;
+select count  (*) from t_sportsman, t_competition tc where tc.competition_name like('%Региональные%') and sportsman_name like('%Иванов%');
+select city from t_result, t_competition tc where tc.world_record = "result" 
+select min("rank") from t_sportsman, t_competition where personal_record = world_record;
+select competition_name  from t_competition group by competition_nameorder by count(*) desc limit 1;
+select country  from t_sportsman group by country order by count(*) desc limit 1;
+update t_sportsman set "rank" = 1 from t_competition where personal_record = world_record;
+update t_competition t1 set set_date =set_date  +interval '4 day' from t_result t2 where t1.competition_id = t2.competition_id  and city = 'Москва';
+update t_sportsman  set country = 'Россия' where "rank" = 1 or "rank" = 2 and country  = 'Италия';
+update t_competition set competition_name = 'Бег с препятствиями' where competition_name = 'Бег';
+update t_result set "result" = ("result" - 2 ) where "result" > 45 and hold_date = '2012-05-20';
+delete from t_result t1 using t_sportsman t2 where t1.sportsman_id  = t2.sportsman_id and t1.city = 'Москва' and t2.year_of_birth <= 1980;
+delete from t_competition t1 using t_result t2 where t1.competition_id  = t2.competition_id and "result" = 20;
+delete from t_result t1 using t_sportsman t2 where t1.sportsman_id  = t2.sportsman_id and t2.year_of_birth = 1980;
